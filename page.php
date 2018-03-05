5b78513dc2d0d18a2d4fef73417e3be2ebe83f85
Andrew Minion <andrew@andrewrminion.com>
2018-03-05 12:24:22 -0500

    remove featured image from page.php

--- page.php
+++ page.php
@@ -21,12 +21,6 @@ get_header(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
-						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-						<div class="entry-thumbnail">
-							<?php the_post_thumbnail(); ?>
-						</div>
-						<?php endif; ?>
-
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 					</header><!-- .entry-header -->
 
